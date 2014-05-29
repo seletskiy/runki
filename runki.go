f49b8e5a486a24512f082ec6cc16396f5cf8ce88
Stanislav Seletskiy <s.seletskiy@gmail.com>
2014-05-29 16:50:17 +0700

    major usability changes
    * --dry flag added;
    * --cut flag added;
    * extract less synonyms and more different meanings;
    * do not crash when no translation found;
    * do not crash on phrases with space;
    * perform morphological search;

--- runki.go
+++ runki.go
@@ -16,15 +16,17 @@ type LookupResult struct {
 
 var lang = flag.String("lang", "en-ru", "translation direction")
 var creds = flag.String("creds", os.Getenv("HOME")+"/.runki/creds",
-	"path to *creds file")
+	"path to creds file")
 var user = flag.String("user", "", "ankiweb username")
 var pass = flag.String("pass", "", "ankiweb password")
 var deck = flag.String("deck", "english", "deck to add")
+var dry = flag.Bool("dry", false, "dry run (do not alter anki db)")
+var cut = flag.Int("cut", 0, "stop processing after N non-unique words found")
 
 func main() {
 	flag.Parse()
 
-	ya := NewYandexProvider(*lang, "")
+	ya := NewYandexProvider(*lang, "", 2)
 	anki := NewAnkiAccount()
 
 	err := anki.Load(*creds)
@@ -42,6 +44,7 @@ func main() {
 	}
 
 	stdin := bufio.NewReader(os.Stdin)
+	foundStreak := 0
 	for {
 		line, err := stdin.ReadString('\n')
 		if err != nil {
@@ -55,10 +58,19 @@ func main() {
 			log.Fatalf(err.Error())
 		}
 
+		if lookup == nil {
+			fmt.Println("<" + unknown + ": no translation found>")
+			continue
+		}
+
 		translation := "[" + lookup.Transcript + "] " +
 			strings.Join(lookup.Meanings, ", ")
 
 		fmt.Println(translation)
+
+		if *dry {
+			continue
+		}
 
 		found, err := anki.Search(unknown)
 		if err != nil {
@@ -66,7 +78,15 @@ func main() {
 		}
 
 		if !found {
+			foundStreak = 0
 			anki.Add(*deck, unknown, translation)
+		} else {
+			foundStreak += 1
+		}
+
+		if foundStreak >= *cut && *cut > 0 {
+			log.Fatalf("stopping after %d consequent non-unique words", *cut)
+			break
 		}
 	}
 
--- yandex.go
+++ yandex.go
@@ -6,34 +6,39 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 )
 
+// flags=4 is for morphological search
 const YA_API_URI = "https://dictionary.yandex.net" +
-	"/api/v1/dicservice.json/lookup?key=%s&lang=%s&text=%s"
+	"/api/v1/dicservice.json/lookup?key=%s&lang=%s&text=%s&flags=4"
 const YA_API_KEY = "dict.1.1.20140512T122957Z.549af1de13649236." +
 	"74bbc11e0fa7625166dd95f21b1ff17838df2c03"
 
 type YandexProvider struct {
-	key  string
-	lang string
+	key           string
+	lang          string
+	limitSynonyms int
 }
 
-func NewYandexProvider(lang, key string) *YandexProvider {
+func NewYandexProvider(lang, key string, limitSynonyms int) *YandexProvider {
 	if key == "" {
 		key = YA_API_KEY
 	}
-	return &YandexProvider{key, lang}
+	return &YandexProvider{key, lang, limitSynonyms}
 }
 
 func (y YandexProvider) Lookup(text string) (*LookupResult, error) {
-	resp, err := http.Get(fmt.Sprintf(YA_API_URI, y.key, y.lang, text))
+	url := fmt.Sprintf(YA_API_URI, y.key, y.lang, url.QueryEscape(text))
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 
 	if resp.Status != "200 OK" {
 		return nil, errors.New(
-			"expected HTTP status 200, received %s" + resp.Status)
+			"expected HTTP status 200, received " + resp.Status +
+				"(" + url + ")")
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
@@ -51,15 +56,27 @@ func (y YandexProvider) Lookup(text string) (*LookupResult, error) {
 		}
 	}{}
 
-	json.Unmarshal(body, &dictResult)
+	err = json.Unmarshal(body, &dictResult)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(dictResult.Def) == 0 {
+		return nil, nil
+	}
 
 	lookupResult := LookupResult{
 		Transcript: dictResult.Def[0].Ts,
-		Meanings:   make([]string, len(dictResult.Def)),
+		Meanings:   make([]string, 0),
 	}
 
-	for i, d := range dictResult.Def {
-		lookupResult.Meanings[i] = d.Tr[0].Text
+	for _, d := range dictResult.Def {
+		for j, tr := range d.Tr {
+			if j >= y.limitSynonyms {
+				break
+			}
+			lookupResult.Meanings = append(lookupResult.Meanings, tr.Text)
+		}
 	}
 
 	return &lookupResult, nil
